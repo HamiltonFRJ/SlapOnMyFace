26f59822a51ecd53073f5ecba0d0d9e0ec73a11b
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-07-06 20:29:04 -0300

    Aprendizado React

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('Hello World');
+
+function App(){
+    return <div>
+        <h1>Aplicativo React</h1>
+        <Button />
+    </div>;
+}
+
+function Button(){
+    const [total, setTotal] = React.useState(0);
+    return <button onClick={() => setTotal(total + 1)}>Adicionar {total}</button>;
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />)
